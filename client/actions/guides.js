a6449bc4e4b22c1c54b79e43f95fb5faaf64c839
Marcelo Jara <marjara35@gmail.com>
2017-10-12 01:22:07 -0300

    User create guide.

--- client/actions/guides.js
+++ client/actions/guides.js
@@ -2,8 +2,8 @@ import fetch from 'isomorphic-fetch';
 import {normalize} from 'normalizr';
 
 import {API_URL} from '../api';
-import {guide} from "../schema";
-import {GUIDE_FETCH, GUIDE_RECEIVE} from "./index";
+import {guide} from '../schema';
+import {GUIDE_FETCH, GUIDE_RECEIVE, GUIDE_SEND} from './index';
 
 export const getGuideById = (id) => (dispatch) => {
   dispatch({
@@ -11,6 +11,29 @@ export const getGuideById = (id) => (dispatch) => {
   });
 
   return fetch(`${API_URL}/material/guides/${id}/`)
+    .then(
+      response => response.json(),
+      error => console.log(error)
+    )
+    .then(response => dispatch({
+      type: GUIDE_RECEIVE,
+      payload: normalize(response, guide)
+    }));
+};
+
+export const sendGuide = (token, guide) => (dispatch) => {
+  dispatch({
+    type: GUIDE_SEND
+  });
+
+  return fetch(`${API_URL}/material/guides/`, {
+    method: 'POST',
+    body: JSON.stringify(guide),
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    }
+  })
     .then(
       response => response.json(),
       error => console.log(error)
