2ef805642a47b023441a54d18c7dcbc82b6b5aa1
Marcelo Jara <marjara35@gmail.com>
2017-10-12 00:33:03 -0300

    Store new comments.

--- client/reducers/exercises.js
+++ client/reducers/exercises.js
@@ -1,10 +1,17 @@
-import {UNIT_RECEIVE, EXERCISE_RECEIVE} from '../actions/index';
+import {UNIT_RECEIVE, EXERCISE_RECEIVE, EXERCISE_COMMENT_RECEIVE} from '../actions/index';
 
 export default (state = {}, action) => {
   switch (action.type) {
     case UNIT_RECEIVE:
     case EXERCISE_RECEIVE:
       return {...state, ...action.payload.entities.exercises};
+    case EXERCISE_COMMENT_RECEIVE:
+      // TODO: change this!!!
+      let exercise = state[action.payload.exercise] ;
+      let comments = exercise.comments;
+      comments = [...comments, action.payload];
+      exercise = {...exercise, comments: comments};
+      return {...state, [exercise.id]: exercise};
   }
   return state;
 }
