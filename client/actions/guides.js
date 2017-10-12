802bafacb6857bab1bdf3fe383eaa63a945ab4c2
Marcelo Jara <marjara35@gmail.com>
2017-10-12 01:48:46 -0300

    Add guideitems request and fix guide links.

--- client/actions/guides.js
+++ client/actions/guides.js
@@ -3,7 +3,7 @@ import {normalize} from 'normalizr';
 
 import {API_URL} from '../api';
 import {guide} from '../schema';
-import {GUIDE_FETCH, GUIDE_RECEIVE, GUIDE_SEND} from './index';
+import {GUIDE_FETCH, GUIDE_ITEM_RECEIVE, GUIDE_ITEM_SEND, GUIDE_RECEIVE, GUIDE_SEND} from './index';
 import {getUserData} from './auth';
 
 export const getGuideById = (id) => (dispatch) => {
@@ -46,5 +46,27 @@ export const sendGuide = (token, guide) => (dispatch) => {
         type: GUIDE_RECEIVE,
         payload: normalize(response, guide)
       })
+    });
+};
+
+export const sendGuideItem = (token, item) => (dispatch) => {
+  dispatch({
+    type: GUIDE_ITEM_SEND,
+  });
+
+  return fetch(`${API_URL}/material/guideitems/`, {
+    method: 'POST',
+    body: JSON.stringify(item),
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    }
+  })
+    .then(
+      response => response.json(),
+      error => console.log(error)
+    )
+    .then(response => {
+      return dispatch(getGuideById(response.guide));
     });
 };
