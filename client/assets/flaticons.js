35e5a4607b1c48b74677acd73b7a673c9b33358b
mijara <marjara35@gmail.com>
2017-10-28 20:12:51 -0300

    Add new icon.

--- client/assets/flaticons.js
+++ client/assets/flaticons.js
@@ -2,5 +2,6 @@ import exercises from './svg/flaticons/icons2/calculation.svg';
 import guides from './svg/flaticons/icons2/library.svg';
 import materials from './svg/flaticons/icons1/reading-1.svg';
 import notes from './svg/flaticons/icons1/notes.svg';
+import courses from './svg/flaticons/icons2/laptop.svg';
 
-export {exercises, guides, materials, notes};
+export {exercises, guides, materials, notes, courses};
