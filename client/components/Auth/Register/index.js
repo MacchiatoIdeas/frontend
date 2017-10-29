e9e20b73ab5a51692ffaa7374e4547379cb9cfcc
mijara <marjara35@gmail.com>
2017-10-29 16:47:42 -0300

    Remove unused function.

--- client/components/Auth/Register/index.js
+++ client/components/Auth/Register/index.js
@@ -20,40 +20,6 @@ export default class Register extends React.Component {
     this.state = {};
   }
 
-  onOptionSelect(form) {
-    this.setState({
-      form
-    });
-  }
-
-  getForm() {
-    if (!this.state.form) {
-      return null;
-    } else if (this.state.form === 'student') {
-      return (
-        <Form title="Estudiante" onBackClick={() => {
-          this.onOptionSelect(undefined);
-        }}>
-          <input type="text" className="form-control" placeholder="nombres" autoFocus required/>
-          <input type="text" className="form-control" placeholder="apellidos" required/>
-
-          <button className="btn btn-block btn-primary">Siguiente Paso</button>
-        </Form>
-      )
-    } else if (this.state.form === 'teacher') {
-      return (
-        <Form title="Profesor" onBackClick={() => {
-          this.onOptionSelect(undefined);
-        }}>
-          <input type="text" className="form-control" placeholder="nombres" autoFocus required/>
-          <input type="text" className="form-control" placeholder="apellidos" required/>
-
-          <button className="btn btn-block btn-primary">Siguiente Paso</button>
-        </Form>
-      )
-    }
-  }
-
   render() {
     const {auth} = this.props;
 
@@ -78,41 +44,3 @@ export default class Register extends React.Component {
     )
   }
 }
-
-/*
-<div>
-  <Navbar backgroundColor="rgba(255, 255, 255)"/>
-
-  <div className="container">
-    <div className="row">
-      <div className="col-sm-6 col-sm-offset-3">
-        <Header icon={icons.register} color="#1A91A1">Crear una cuenta</Header>
-
-        {this.state.form ? null : (
-          <div className={`panel panel-default ${style.panel}`}>
-            <div className={`panel-body ${style.panelBody}`}>
-              <div className="row">
-                <div className="col-sm-12">
-                  <h3 className={style.title}>Genial!, cómo quiere usar nuestra plataforma?</h3>
-                </div>
-
-                <div className="row">
-                  <OptionButton text="Estudiante" icon="education" onClick={() => {
-                    this.onOptionSelect('student');
-                  }}/>
-
-                  <OptionButton text="Profesor" icon="blackboard" onClick={() => {
-                    this.onOptionSelect('teacher');
-                  }}/>
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
-
-        {this.getForm()}
-      </div>
-    </div>
-  </div>
-</div>
- */
