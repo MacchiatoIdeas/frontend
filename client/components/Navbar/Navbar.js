84c04fc85bf29327f0f00c13eeac92e0f3d7f05e
Hernán Herreros Niño <hernan.herreros.13@sansano.usm.cl>
2017-10-29 00:20:05 -0300

    Implement search section

--- client/components/Navbar/Navbar.js
+++ client/components/Navbar/Navbar.js
@@ -5,6 +5,8 @@ import {NavLink} from 'react-router-dom';
 import logo from '../../assets/img/logo.png';
 import * as icons from '../../assets/flaticons';
 import style from './Navbar.less';
+import ReactLoading from 'react-loading';
+import Box from '../Box';
 
 export default class Navbar extends React.Component {
   constructor(props) {
@@ -12,24 +14,31 @@ export default class Navbar extends React.Component {
     this.state = {
       searchOpen: false,
       searchInput: "",
+      searching: false,
+      showing: false,
+      listResult: [],
     };
     this.openSearch = this.openSearch.bind(this);
     this.closeSearch = this.closeSearch.bind(this);
     this.onInputChange = this.onInputChange.bind(this);
+    this.search = this.search.bind(this);
   }
 
   openSearch() {
     this.setState({
       searchOpen: true,
     });
-
-    this.input.focus();
+    setTimeout(() => {
+      this.input.focus();
+    }, 100);
   }
 
   closeSearch() {
     this.setState({
       searchOpen: false,
       searchInput: "",
+      searching: false,
+      showing: false,
     });
   }
 
@@ -47,9 +56,98 @@ export default class Navbar extends React.Component {
     };
   }
 
+  search(ev) {
+    if (ev.keyCode === 13) {
+      this.setState({
+        searching: this.state.searchInput !== "",
+        showing: false,
+      });
+      if (this.state.searchInput !== "") {
+        setTimeout(() => {
+          this.searchRequest()
+        }, 1000);
+      }
+    }
+  }
+
+  searchRequest() {
+
+    let list = [
+      {
+        'title': 'Primer elemento de la búsqueda',
+        'link': '#',
+        'author': {
+          'id': 1,
+          'first_name': 'Hernán',
+          'last_name': 'Herreros Niño',
+        },
+        'text': 'Texto del primer elemento de la búsqueda',
+        'date': '28 de Octubre de 2017',
+        'comments': '10',
+        'linkText': undefined,
+      }, {
+        'title': 'Segundo elemento de la búsqueda',
+        'link': '#',
+        'author': {
+          'id': 2,
+          'first_name': 'Marcelo',
+          'last_name': 'Jara Almeyda',
+        },
+        'text': 'Texto del segundo elemento de la búsqueda',
+        'date': '28 de Octubre de 2017',
+        'comments': '12',
+        'linkText': undefined,
+      }, {
+        'title': 'Tercer elemento de la búsqueda',
+        'link': '#',
+        'author': {
+          'id': 3,
+          'first_name': 'Rodolfo',
+          'last_name': 'Castillo Mateluna',
+        },
+        'text': 'Texto del tercer elemento de la búsqueda',
+        'date': '28 de Octubre de 2017',
+        'comments': '12',
+        'linkText': undefined,
+      }, {
+        'title': 'Tercer elemento de la búsqueda',
+        'link': '#',
+        'author': {
+          'id': 3,
+          'first_name': 'Rodolfo',
+          'last_name': 'Castillo Mateluna',
+        },
+        'text': 'Texto del tercer elemento de la búsqueda',
+        'date': '28 de Octubre de 2017',
+        'comments': '12',
+        'linkText': undefined,
+      }, {
+        'title': 'Tercer elemento de la búsqueda',
+        'link': '#',
+        'author': {
+          'id': 3,
+          'first_name': 'Rodolfo',
+          'last_name': 'Castillo Mateluna',
+        },
+        'text': 'Texto del tercer elemento de la búsqueda',
+        'date': '28 de Octubre de 2017',
+        'comments': '12',
+        'linkText': undefined,
+      }
+    ];
+
+    this.setState({
+      searching: false,
+      showing: true,
+      listResult: list,
+    })
+  }
+
   render() {
     let transparentClass = this.props.transparent ? style.transparent : "";
     let searchStyle = this.state.searchOpen ? style.navbarSearch : "";
+    let loading = this.state.searching ? style.isLoading : "";
+    let showing = this.state.showing ? style.isShowing : "";
 
     return (
       <nav id="navbar" className={`navbar navbar-default ${transparentClass} ${style.navbar} ${searchStyle}`}>
@@ -83,37 +181,51 @@ export default class Navbar extends React.Component {
           <div className={style.searchBody}>
             <input ref={(input) => {
               this.input = input;
-            }} value={this.state.searchInput} onChange={this.onInputChange} type="text"
-                   className={`${style.inputSearch}`} placeholder="Inserte su búsqueda aquí"/>
+            }} value={this.state.searchInput} onChange={this.onInputChange} onKeyDown={this.search} type="text"
+                   placeholder="Inserte su búsqueda aquí" tabIndex="-1"/>
             <button onClick={this.closeSearch} className={style.closeSearch}><span
               className="glyphicon glyphicon-remove"/></button>
           </div>
         </div>
-        <div className={style.resultsBody}>
+        <div className={`${style.resultsBody} ${loading} ${showing} clearfix`}>
           <div className={style.placeholder}>
-            <div className="col-sm-8 col-sm-offset-2 text-center">
-              <p className="lead text-muted">¿Qué estás buscando?</p>
-              <div className="col-sm-12">
-                <div className="col-xs-4">
-                  <div className={style.item}>
-                    <img src={icons.exercises} alt=""/>
-                    <p>Ejercicios</p>
+            <div className="container">
+              <div className="col-sm-8 col-sm-offset-2 text-center">
+                <p className="lead text-muted">¿Qué estás buscando?</p>
+                <div className="col-sm-12">
+                  <div className="col-xs-4">
+                    <div className={style.item}>
+                      <img src={icons.exercises} alt=""/>
+                      <p>Ejercicios</p>
+                    </div>
                   </div>
-                </div>
-                <div className="col-xs-4">
-                  <div className={style.item}>
-                    <img src={icons.guides} alt=""/>
-                    <p>Guías</p>
+                  <div className="col-xs-4">
+                    <div className={style.item}>
+                      <img src={icons.guidesv2} alt=""/>
+                      <p>Guías</p>
+                    </div>
                   </div>
-                </div>
-                <div className="col-xs-4">
-                  <div className={style.item}>
-                    <img src={icons.materials} alt=""/>
-                    <p>Material</p>
+                  <div className="col-xs-4">
+                    <div className={style.item}>
+                      <img src={icons.materials} alt=""/>
+                      <p>Material</p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
+          </div>
+          <div className={style.loading}>
+            <div className="col-sm-8 col-sm-offset-2 text-center">
+              <ReactLoading type="spin" color="#666" className={style.spinner}/>
+              <p>Un momento</p>
+            </div>
+          </div>
+          <div className={style.list}>
+            {this.state.listResult.map((item, key) =>
+              <Box key={key} title={item.title} link={item.title} text={item.text} author={item.author} date={item.date}
+                   comments={item.comments} linkText={item.linkText}/>
+            )}
           </div>
         </div>
       </nav>
