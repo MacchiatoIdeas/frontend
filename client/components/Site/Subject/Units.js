d9e847f7408ef8a237f58d673ef01c3d8796b80e
Marcelo Jara <marjara35@gmail.com>
2017-11-09 22:24:35 -0300

    Add count of documents and exercise per unit.

--- client/components/Site/Subject/Units.js
+++ client/components/Site/Subject/Units.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+import style from './style.less';
+
 const Units = ({units}) =>
   <div className="list-group">
     {units.map((unit, i) =>
       <Link to={`/site/units/${unit.id}`} key={unit.id} className="list-group-item">
         {unit.name}
+
+        <span className={style.Badge}>
+          {unit.ncontents} <span className="glyphicon glyphicon-play" style={{fontSize: 12}}/>
+        </span>
+
+        <span className={style.Badge}>
+          {unit.nexercises} <span className="glyphicon glyphicon-file" style={{fontSize: 12}}/>
+        </span>
       </Link>
     )}
   </div>;
